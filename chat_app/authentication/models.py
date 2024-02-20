badbe2cead6df117de80f4c67bd8068a2d94c093
Haikal Fikri <haikaljr1245@gmail.com>
2024-02-20 16:29:41 +0700

    update model

--- chat_app/authentication/models.py
+++ chat_app/authentication/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    USER_ROLE_CHOICES = [
+        ('user', 'User'),
+        ('admin', 'Admin'),
+    ]
+    
+    role = models.CharField(max_length=10, choices=USER_ROLE_CHOICES, default='user')
